fe98749dcb79231c8dac273d51751935bea75ac9
thong1403 <110025148+thong1403@users.noreply.github.com>
2023-12-27 20:52:54 +0700

    Update Home-Page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import "./App.css";
-import Registration from "./pages/Registration/Registration";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import HomePages from "./pages/Login/homePages";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePages from "./pages/homePages/homePages";
+import Headers from "./components/header/Header";
+import Footer from "./components/footer/Footer";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-      
-      <Routes>
-      <Route path="/pages/home" element = {<HomePages/>}></Route>
-      <Route path="/user/register" element={  <Registration />}></Route>
-      </Routes>
-    
+      <Headers />
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+              <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePages />}></Route>
+        </Routes>
       </BrowserRouter>
+          </div>
+        </div>
+      </div>
+    
 
+      <Footer/>
     </div>
   );
 }

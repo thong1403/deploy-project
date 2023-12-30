eb2b2f131e208356fc59bda4017bb86c7d146d19
thong1403 <110025148+thong1403@users.noreply.github.com>
2023-12-30 16:23:44 +0700

    Update Home

--- src/App.js
+++ src/App.js
@@ -4,24 +4,27 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePages from "./pages/homePages/homePages";
 import Headers from "./components/header/Header";
 import Footer from "./components/footer/Footer";
+import SideBar from "./components/sideBar/SideBar";
 function App() {
   return (
     <div className="App">
       <Headers />
       <div className="container">
         <div className="row">
-          <div className="col-12">
-              <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePages />}></Route>
-        </Routes>
-      </BrowserRouter>
+          <div className="col-9">
+            <BrowserRouter>
+              <Routes>
+                <Route path="/" element={<HomePages />}></Route>
+              </Routes>
+            </BrowserRouter>
+          </div>
+          <div className="col-3">
+            <SideBar />
           </div>
         </div>
       </div>
-    
 
-      <Footer/>
+      <Footer />
     </div>
   );
 }
